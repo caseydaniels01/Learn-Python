273fa1bb8607157b5737227976a7e603005163d9
caseydaniels01 <caseydaniels01@gmail.com>
2023-12-08 23:40:44 -0600

    Update Pomodoro.py

--- Arguments-And-TKINTER/Pomodoro.py
+++ Arguments-And-TKINTER/Pomodoro.py
@@ -6,17 +6,22 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-
-
 def reset_button_click():
-    pass
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    checkmark.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -66,9 +71,14 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count -1)
+        global timer
+        timer = window.after(1000, count_down, count -1)
     else:
         start_button_click()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark += "✔"
+        checkmark.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -93,7 +103,11 @@ button_reset.grid(column=2, row=2)
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
 timer_label.grid(column=1, row=0)
 
-checkmark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+checkmark = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
 checkmark.grid(column=1, row=3)
 
 window.mainloop()
+
+
+
+#checkmark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
