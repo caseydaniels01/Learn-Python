7b3a3c77e961d893b61f2ff2e80af060355b0908
caseydaniels01 <caseydaniels01@gmail.com>
2023-12-10 22:32:34 -0600

    Exceptions and JSON

--- Dictionary-Comprehension/NATO_Pandas.py
+++ Dictionary-Comprehension/NATO_Pandas.py
@@ -31,6 +31,14 @@ import pandas
 data = pandas.read_csv(r"c:\Users\casey\OneDrive\Desktop\Code - VSCode\Learn-Python\Learn-Python\Dictionary-Comprehension\nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
