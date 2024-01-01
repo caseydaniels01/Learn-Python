f34a47ba280ab49f644b57253656a141221eb8b0
caseydaniels01 <caseydaniels01@gmail.com>
2023-12-31 21:47:34 -0600

    Monday_Motivation_email

---
+++ Email/Monday_Motivation.py
@@ -0,0 +1,53 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "email@gmail.com"
+MY_PASSWORD = "password"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
+
+
+## Sending Email with Python
+# import smtplib
+#
+# my_email = "email@gmail.com"
+# password = "password"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="email@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
+
+
+## Working with date and time in Python
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+# date_of_birth = dt.datetime(year=1901, month=1, day=1, hour=1)
+# print(date_of_birth)
